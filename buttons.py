d71d268d624ce5fcc6f7b909d1ef5c7d302fec03
ikace <vkonstantunov@yandex.ru>
2022-04-12 13:42:07 +0300

    initial commit

--- buttons.py
+++ buttons.py
@@ -4,26 +4,20 @@ from aiogram.types import ReplyKeyboardRemove, \
 
 
 button_users = KeyboardButton('Пользователи')
-menu = KeyboardButton('Меню')
-basket = KeyboardButton('Добавить в корзину')
-replykb = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2).add(menu)
-replykb1 = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2).add(basket)
+restoran1 = KeyboardButton('Шоколадница')
+restoran2 = KeyboardButton('Кофе Хауз')
+restoran3 = KeyboardButton('Ваби-Саби')
+replykb = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2).add(restoran1,restoran2,restoran3)
+replykb1 = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2).add(restoran1)
 
 
 
 
-def genmarkup(data): # передаём в функцию data
-
-    markup = InlineKeyboardMarkup() # создаём клавиатуру
-    markup.row_width = 1 # кол-во кнопок в строке
-    for i in data: # цикл для создания кнопок
-        markup.add(InlineKeyboardButton(i[1], callback_data=i[2]))
-    return markup #возвращаем клавиатуру
 
 
-def basket(data):
-    markup = InlineKeyboardMarkup()  # создаём клавиатуру
-    markup.row_width = 1  # кол-во кнопок в строке
-    for i in data:  # цикл для создания кнопок
-        markup.add(InlineKeyboardButton(i[1], callback_data=i[2]))
-    return markup  # возвращаем клавиатуру
+#def basket(data):
+    #markup = InlineKeyboardMarkup()  # создаём клавиатуру
+    #markup.row_width = 1  # кол-во кнопок в строке
+    #for i in data:  # цикл для создания кнопок
+        #markup.add(InlineKeyboardButton(i[1], callback_data=i[2]))
+    #return markup  # возвращаем клавиатуру
--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import logging
+import buttons
 from config import TOKEN
 
 
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
 
 
 @dp.message_handler(commands="start")
 async def process_start_command(message: types.Message):
-    await message.reply("Привет!\nНапиши мне что-нибудь!")
+    await message.answer("Выберите нужный вам ресторан",reply_markup=buttons.replykb)
+    print("Нажата кнопка старт")
 
 
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    executor.start_polling(dp,skip_updates=True)
